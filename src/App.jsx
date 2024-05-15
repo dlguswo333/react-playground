5c5820110352a7cc3d9bdb28b8781c07bdbd511f
dlguswo333 <dlguswo002@gmail.com>
2024-05-15 20:53:18 +0900

    Add preact signal

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import {useState} from 'react';
+import {signal} from '@preact/signals-react';
+import {useCallback} from 'react';
+
+const Child = () => {
+  console.log('Child rerender');
+  return <div>child</div>;
+};
+
+const count = signal(0);
+
+const Counter = ({children}) => (
+  <div>
+    Count: {count}
+    <br />
+    {children}
+  </div>
+);
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  // const count = useSignal(0);
+  console.log('App rerender');
+
+  const cb = useCallback(() => {
+    console.log('before:', count.value);
+    count.value++;
+    console.log('after:', count.value);
+  }, []);
 
   return <>
     <div>
       App count: {count}
     </div>
-    <button onClick={() => setCount(count + 1)}>Click</button>
+    <Counter>
+      {count}
+    </Counter>
+    <button onClick={() => count.value++}>Click</button>
+    <button onClick={cb}>Callback</button>
+    <Child />
   </>;
 };
 
